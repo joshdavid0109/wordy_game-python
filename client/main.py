8785000a24b6c5d69866d125f3556d6597c25a95
darren franz e domantay <2225292@slu.edu.ph>
2023-05-04 20:25:57 +0800

    medyo gumagana na (main.py) fix things lang

--- client/main.py
+++ client/main.py
@@ -5,14 +5,14 @@ from tkinter.ttk import *
 from PIL import Image, ImageTk
 
 
-class logIn(tk.Frame):
+class LogIn(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         Label = tk.Label(self, text="LogIn")
         Label.place(x=230, y=230)
 
-        Button = tk.Button(self, text="next", command=lambda: controller.show_frame(MainMenu))
-        Button.place(x=650, y=450)
+        Button = tk.Button(self, text="next", font=("Arial", 15), command=lambda: controller.show_frame(MainMenu))
+        Button.place(x=400, y=300)
 
 
 class MainMenu(tk.Frame):
@@ -32,7 +32,7 @@ class Game(tk.Frame):
         Label = tk.Label(self, text="Game")
         Label.place(x=230, y=230)
 
-        Button = tk.Button(self, text="next", command=lambda: controller.show_frame(Game))
+        Button = tk.Button(self, text="next", command=lambda: controller.show_frame(LogIn))
         Button.place(x=650, y=450)
 
 
@@ -47,14 +47,17 @@ class Application(tk.Tk):
         window.grid_columnconfigure(0, minsize=800)
 
         self.frames = {}
-        for F in (logIn, MainMenu, Game):
+        for F in (LogIn, MainMenu, Game):
             frame = F(window, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(logIn)
+        self.show_frame(LogIn)
 
-        def show_frame(self,page):
-            frame = self.frames[page]
-            frame.tkraise
+    def show_frame(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
 
+
+app = Application()
+app.mainloop()
