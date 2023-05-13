862c5f1e84daa6eede7e4f6abf1321e8e4217f3b
joshdavid0109 <2222551@slu.edu.ph>
2023-05-13 16:17:59 +0800

    working na corba connection to java. pwede na simulan
    * yung paginvoke ng method kailangan pala manually itype yung method name

--- ClientApp.py
+++ ClientApp.py
@@ -3,11 +3,14 @@ from omniORB import CORBA
 import WordyGame
 
 # APP CONFIG
+# Manually pagtype ng method pag first time iinvoke
 # -ORBInitRef NameService=corbaname::localhost:9999
 
 orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
 obj = orb.string_to_object("corbaname::localhost:9999#Hello")  # default corba object name::{host}:{port}
 
-eo = obj._narrow(WordyGame.WordyGameServer)
+wordyGameServer = obj._narrow(WordyGame.WordyGameServer)
 
-print(eo.login("admin", "pass"))
+print(wordyGameServer.login("admin", "pass"))
+
+wordyGameServer.playGame();
